cf40ed88ea532597c600d5cbec49af47c06bbe3d
trpdjke <yehorushakov@gmail.com>
2026-03-03 23:49:12 +0200

    delete details from unauthorized

--- headers/account_auth.go
+++ headers/account_auth.go
@@ -14,12 +14,12 @@ const (
 func GetAuthorizationToken(r *http.Request) (string, error) {
 	authHeader := r.Header.Get(AuthorizationHeader)
 	if authHeader == "" {
-		return "", problems.Unauthorized("Missing Authorization header")
+		return "", problems.Unauthorized()
 	}
 
 	parts := strings.Split(authHeader, " ")
 	if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
-		return "", problems.Unauthorized("Missing Authorization header")
+		return "", problems.Unauthorized()
 	}
 
 	return parts[1], nil
--- problems/internal_error.go
+++ problems/internal_error.go
@@ -13,9 +13,6 @@ func InternalError() error {
 		Title:  http.StatusText(http.StatusInternalServerError),
 		Status: fmt.Sprintf("%d", http.StatusInternalServerError),
 		Code:   "INTERNAL_SERVER_ERROR",
-		Detail: "Oops, an unexpected error has occurred." +
-			" We are already looking into it and doing everything we can to ensure that you don't see it again." +
-			" Please come back soon.",
 		Meta: &map[string]any{
 			"timestamp": time.Now().UTC(),
 		},
